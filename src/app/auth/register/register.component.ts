10530933156982cc8f66e559f2f861ecbfd489e9
Ikadev95 <fegughi@gmail.com>
2024-10-25 13:54:34 +0200

    register better validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,25 +1,52 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthsrvService } from '../authsrv.service';
 import { iUser } from '../../interfaces/i-user';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'] // Corretto 'styleUrl' in 'styleUrls'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-  formData:Partial<iUser> = {}
+  form: FormGroup; // FormGroup per gestire il modulo
+  formvalid = false
 
-  constructor(private authSvc : AuthsrvService, private router:Router){}
-
-  register(){
-    this.authSvc.register(this.formData)
-    .subscribe(res => {
-      this.router.navigate(['auth/']);
-    })
-    console.log('registrazione andata a buon fine')
+  constructor(private authSvc: AuthsrvService, private router: Router, private fb: FormBuilder) {
+    this.form = this.fb.group({ // Crea il FormGroup con i controlli e le validazioni
+      name: ['', Validators.required], // Nome è richiesto
+      email: ['', [Validators.required, Validators.email]], // Email è richiesta e deve essere in formato email
+      password: ['', Validators.required] // Password è richiesta
+    });
   }
+
+  ngOnInit(): void {}
+
+  register() {
+    if (this.form.valid) { // Verifica se il modulo è valido
+      this.authSvc.register(this.form.value) // Usa registerForm.value per ottenere i dati
+        .subscribe(res => {
+          console.log('Registrazione andata a buon fine');
+          this.router.navigate(['auth/']); // Naviga alla pagina di autenticazione
+        });
+    }
+    else {
+      this.formvalid = true
+      console.log("not valid")
+    }
+  }
+
+  isValid(fieldName: string) {
+    return this.form.get(fieldName)?.valid
+   }
+   isTouched(fieldName: string) {
+     return this.form.get(fieldName)?.touched
+   }
+
+  isInValidTouched(fieldName:string){
+    return !this.isValid(fieldName) && this.isTouched(fieldName)
+  }
+
 }
