ee515dbf3f40295ae12e7852cfd9c65876a03be9
Ikadev95 <fegughi@gmail.com>
2024-10-27 16:17:13 +0100

    form ok

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,8 @@
 import { AuthsrvService } from './../authsrv.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { iLoginRequest } from '../../interfaces/i-login-request';
-import { NgForm } from '@angular/forms';
-import { map } from 'rxjs';
-
-
 
 @Component({
   selector: 'app-login',
@@ -13,34 +10,37 @@ import { map } from 'rxjs';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  formValid: boolean = true;
 
-  formData:iLoginRequest = {
-    email : '',
-    password : ''
+  constructor(private authSvc: AuthsrvService, private router: Router, private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
-
-  constructor(private authSvc : AuthsrvService, private router:Router){}
 
   ngOnInit(): void {
+
   }
 
-  login(){
-    this.authSvc.login(this.formData)
-    .subscribe(data =>{
-      console.log('ok')
-      this.router.navigate(['films'])
-    })
-  }
-
-  @ViewChild('f') form!:NgForm
-
-    submit(form:NgForm){
-      console.log('form inviato al submit',form);
-      console.log(form.form.value);
-
-      form.reset();
-
+  login() {
+    if (this.loginForm.valid) {
+      const formData: iLoginRequest = this.loginForm.value;
+      this.authSvc.login(formData).subscribe({
+        next: (data) => {
+          console.log('Login successful', data);
+          this.router.navigate(['films']);
+        },
+        error: (err) => {
+          console.error('Login error', err);
+          this.formValid = false;
+        }
+      });
+    } else {
+      this.formValid = false;
+      console.log('Form is invalid');
     }
-
-
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,29 +7,29 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'] // Corretto 'styleUrl' in 'styleUrls'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  form: FormGroup; // FormGroup per gestire il modulo
+  form: FormGroup;
   formvalid = false
 
   constructor(private authSvc: AuthsrvService, private router: Router, private fb: FormBuilder) {
-    this.form = this.fb.group({ // Crea il FormGroup con i controlli e le validazioni
-      name: ['', Validators.required], // Nome è richiesto
-      email: ['', [Validators.required, Validators.email]], // Email è richiesta e deve essere in formato email
-      password: ['', Validators.required] // Password è richiesta
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {}
 
   register() {
-    if (this.form.valid) { // Verifica se il modulo è valido
-      this.authSvc.register(this.form.value) // Usa registerForm.value per ottenere i dati
+    if (this.form.valid) {
+      this.authSvc.register(this.form.value)
         .subscribe(res => {
           console.log('Registrazione andata a buon fine');
-          this.router.navigate(['auth/']); // Naviga alla pagina di autenticazione
+          this.router.navigate(['auth/']);
         });
     }
     else {
